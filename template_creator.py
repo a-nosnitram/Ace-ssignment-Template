04326cd46551a9de55dcc9527c547c2900ff19d8
am780 <am780@klovia.cs.st-andrews.ac.uk>
2025-03-01 01:58:27 +0000

    Java and C project templates

--- template_creator.py
+++ template_creator.py
@@ -1,17 +1,33 @@
 import os
 from pathlib import Path
-from string import Template 
+from string import Template
+# from colorama import Fore, Back, Style
+from datetime import datetime
 
 # report template 
 # to be modified 
-REPORT_TEMPLATE = Template("""# ${module_code}-W${week_number}""")
+REPORT_TEMPLATE = Template("""# ${module_code} W${week_number} Assignment {report_type}
+# {title}
+## Tutor: ${tutor_name}
+## Strudent: ${matriculation}
+### Date: ${date}
+
+## Introduction 
+
+## Design 
+
+## Testing 
+
+## Conclusion """)
 
 def create_project():
     # get user input using input()
-    project_type = input("Project type (Group / Individual) : ").strip().lower()
-    week_number = input("Week number : ")
+    project_type = input_loop("project type (Group (g) / Individual (i))", {"g", "i"})
+    week_number = input("Enter week number : ")
     project_title = input("Enter project title : ")
-    language = input("Enter porgramming language : ").strip()
+    language = input_loop("porgramming language", {"java", "python", "c", 
+                                                   "c++", "haskell", "c#", 
+                                                   "javascript"})
 
     # report details input 
     module_code = input("Enter module code : ")
@@ -23,26 +39,95 @@ def create_project():
     base_path = Path(folder_name) 
     
     # create folder structure 
-    if project_type == "individual":
+    if project_type == "i": # individual report setup
         src_path = base_path / "src"
         report_file = base_path / f"W{week_number}-Report.md"
-    else:
+
+        report_type = ""
+    else: # group project setup
         src_path = base_path / "Code" / "src"
         report_path = base_path / "Reports" 
         report_path.mkdir(parents=True, exist_ok=True)
+        report_type_g = "Group Report"
+
         group_report_file = report_path / "Group-Report.md"
         individual_report_file = report_path / "Individual-Report.md"
+        report_type_i = "Individual Report"
+
+    # programming language specifics 
+    match language: 
+        case "java":
+            main_file = src_path / "main.java"
+            main_file_content = """public class main() {
+            public static void main(String args[]) {
+                System.out.println("Good luck with your coursework ;)");
+            }
+}"""
+            script_content = """#!/bin/bash
+// this is a scrip to run your java project
+// add files to the compile and run sections as needed
+
+// compile your files 
+javac main.java 
+
+// run your files
+java main"""
+            script_file = src_path / "runJava.sh"
+        case "c": 
+            main_file = src_path / "main.c"
+            main_file_content = """ #include <stdio.h> 
+
+                                    int main(int argc, char **argv) {
+                                        putStrLn("Good luck with your coursework ;)\n");
+                                        return 0; 
+                                    }"""
+            script_content = """CC = GCC
+                                 TARGET = main
+                                 CFLAGS = -Wall -Wextra
+                                 OBJS = main.o
+
+                                 $(TARGET): $(OBJS) 
+                                    $(CC) $(CFALGS) -o $(TARGET) $(OBJS)
+
+                                 main.o: main.c 
+                                    $(CC) $(CFLAGS) -c main.c
+
+                                 clean: 
+                                    rm *.o $(TARGET)
+                                 """
+            script_file = src_path / "Makefile"
+        #case "haskell":
+        #case "javscript":
 
     # create directories using Path.mkdir 
     src_path.mkdir(parents=True, exist_ok=True)
+   
+   # configure project files 
+    main_file.write_text(main_file_content)
+    script_file.write_text(script_content)
 
     # define report content using substitute
-    report_contents = REPORT_TEMPLATE.substitute(module_code=module_code, week_number=week_number)
+    if project_type == "i":
+        report_contents = REPORT_TEMPLATE.substitute(module_code=module_code,week_number=week_number, report_type=report_type, tutor_name=tutor_name, matriculation=matriculation, date={datetime.today().strftime('%d %m %Y')})
+        # write report files 
+        report_file.write_text(report_contents)
+    else:
+        report_g_contents = REPORT_TEMPLATE.substitute(module_code=module_code,week_number=week_number,report_type=report_type_g, tutor_name=tutor_name, matriculation=matriculation, date={datetime.today().strftime('%d %m %Y')})
+        group_report_file.write_text(report_g_contents)
 
-    # write report files 
-    report_file.write_text(report_contents)
+        report_i_contents = REPORT_TEMPLATE.substitute(module_code=module_code,week_number=week_number, report_type=report_type_i,tutor_name=tutor_name, matriculation=matriculation, date={datetime.today().strftime('%d %m %Y')})
+        individual_report_file.write_text(report_i_contents)
 
-    print("done.")
+    print("All done :з project created !")
+
+def input_loop(prompt, target_set):
+    inp = input(f"Enter {prompt} : ").strip().lower()
+    while inp not in target_set:
+        print("Invalid input")
+        inp = input(f"Enter {prompt} : ").strip().lower()
+    return inp
+
 
 if __name__ == "__main__":
     create_project()
+
