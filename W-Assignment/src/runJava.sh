adb6b00d96e4d4470913ece5490e5f7f5070cea7
am780 <am780@pc8-043-l.cs.st-andrews.ac.uk>
2025-03-04 14:02:29 +0000

    table linking for md reports

--- W-Assignment/src/runJava.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this is a script to run your java project
-# add files to the compile and run sections as needed
-
-# compile your files
-javac Main.java
-
-# run your files
-java Main
